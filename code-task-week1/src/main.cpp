33059966ed0788c9205e92153d9d3d240f5801da
Ardian109 <71823009+Ardian109@users.noreply.github.com>
2020-09-27 13:34:48 +0700

    Memasukkan coding Tugas 1
    1. ESP32 yang dapat mengontrol LED
    2. ESP32 yang dapat membaca masukkan dari PC lewat komunikasi serial

--- code-task-week1/src/main.cpp
+++ code-task-week1/src/main.cpp
@@ -1,9 +1,52 @@
 #include <Arduino.h>
+#define BUTTON_PIN 18
+#define LED_PIN1 4
+#define LED_PIN2 21
+#define LED_PIN3 2
+
+int led_status = LOW;
 
 void setup() {
   // put your setup code here, to run once:
+  
+  pinMode(LED_PIN1, OUTPUT);
+  pinMode(LED_PIN2, OUTPUT);
+  pinMode(LED_PIN3, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  Serial.begin(115200);
+  delay(1000);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    
+  if (Serial.available()){
+   char data = Serial.read();
+   Serial.println();
+   Serial.print("Karakter yang dikirim: ");
+   Serial.println(data);
+   if (data == '1'){
+     Serial.println("Lampu 1 is ON");
+     digitalWrite(LED_PIN1, HIGH);
+     delay(2000);
+     digitalWrite(LED_PIN1, LOW);
+   }
+   else if (data == '2'){
+     Serial.println("Lampu 2 is ON");
+     digitalWrite(LED_PIN2, HIGH);
+     delay(2000);
+     digitalWrite(LED_PIN2, LOW);
+   }
+   else if (data == '3'){
+     Serial.println("Lampu 3 is ON");
+     digitalWrite(LED_PIN3, HIGH);
+     delay(2000);
+     digitalWrite(LED_PIN3, LOW);
+   }
+   else if (data == '4'){
+     Serial.println("Lampu 4 is ON");
+
+   
+}
+}
 }
